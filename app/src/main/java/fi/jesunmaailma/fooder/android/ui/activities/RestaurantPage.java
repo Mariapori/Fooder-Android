cac77ba21df913adc16f1df650dddc13b8176169
Jesse Keskelä <monkeydie_@hotmail.com>
2022-10-07 19:27:22 +0300

    Hakutoiminto toimii, mutta huonosti.

--- app/src/main/java/fi/jesunmaailma/fooder/android/ui/activities/RestaurantPage.java
+++ app/src/main/java/fi/jesunmaailma/fooder/android/ui/activities/RestaurantPage.java
@@ -393,7 +393,7 @@ public class RestaurantPage extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
-            onBackPressed();;
+            onBackPressed();
         }
         return super.onOptionsItemSelected(item);
     }
